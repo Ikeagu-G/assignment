ca4b47d4f508aaadbb151e2df775765c7bcd4a99
george <georgeikeagu@gmail.com>
2022-03-17 09:23:04 +0100

    assignment

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TdAfrica
+
+  
+{
+    class Trapezium
+    {
+        public double side_a;
+        public double side_b;
+        public double height;
+
+        public Trapezium(double aside_a,double aside_b,double aheight)
+        {
+            side_a = aside_a;
+            side_b = aside_b;
+            height = aheight;
+
+
+        }
+        public double Area()
+        {
+            double tay = 0.5 * height;
+            double Area= tay *(side_a + side_b);
+            return Area;
+        }
+
+
+
+
+    }  
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TdAfrica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trapezium trapezium = new Trapezium(8,9.6,10);
+            Trapezium trapezium1 = new Trapezium(3.4, 5.6, 7.6);
+            Console.WriteLine(trapezium.Area());
+            Console.WriteLine(trapezium1.Area());
+            Console.ReadLine();
+        }
+    }
+}
