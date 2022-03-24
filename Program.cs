19771e9b2b221999dc24effc53cffd5a4b01f204
george <georgeikeagu@gmail.com>
2022-03-24 10:00:30 +0100

    Another assigment

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AssignmentTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Y = Console.ReadLine();
+
+            int X=int.Parse(Y);
+
+            int P = X / 100;
+
+            int Q = P / 10;
+
+            
+
+            var S= Y[1] == 7 && Q == 7 ? "You are correct" : "You are wrong";
+
+            Console.WriteLine(X);
+
+            Console.WriteLine(P);
+
+            Console.WriteLine(Q);
+
+            Console.WriteLine(S);
+            
+
+        }
+    }
+}
